082c19dd3b2f989dc990354182332f602bf56d61
rkdals213 <rkdals213@naver.com>
2020-12-23 23:35:56 +0900

    h

--- src/main/kotlin/jpabook/jpashop/jpakotlin/repository/query/OrderQueryDto.kt
+++ src/main/kotlin/jpabook/jpashop/jpakotlin/repository/query/OrderQueryDto.kt
@@ -1,7 +1,6 @@
 package jpabook.jpashop.jpakotlin.repository.query
 
 import jpabook.jpashop.jpakotlin.domain.Address
-import jpabook.jpashop.jpakotlin.domain.OrderItem
 import jpabook.jpashop.jpakotlin.domain.OrderStatus
 import java.time.LocalDateTime
 import java.util.ArrayList
@@ -17,5 +16,21 @@ class OrderQueryDto(
 
     constructor(id: Long, name: String, orderDate: LocalDateTime, status: OrderStatus, address: Address, orderItems: List<OrderItemQueryDto>) : this(id, name, orderDate, status, address) {
         this.orderItems = orderItems
+    }
+
+    // map으로 그루핑할때 기준점이 됨
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as OrderQueryDto
+
+        if (id != other.id) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return id.hashCode()
     }
 }
